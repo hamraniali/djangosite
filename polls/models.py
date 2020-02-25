183968b3be0564253bfc1bfa53ee7bacb38acb8f
hamraniali <52961262+hamraniali@users.noreply.github.com>
2020-02-25 18:27:21 +0330

    add new app names polls

---
+++ polls/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Question(models.Model):
+    questionText = models.CharField(max_length=255)
+    pubDate = models.DateTimeField('date published')
+    def __str__(self):
+        return self.questionText
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question , on_delete=models.CASCADE)
+    choiceText = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choiceText
